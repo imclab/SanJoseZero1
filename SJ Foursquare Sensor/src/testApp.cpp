ab9fb88725ec12ded2e02ac64f21ab7e8555d8e3
robotconscience <brett@robotconscience.com>
2010-06-30 10:44:05 -0400

    added foursquare sensor + osc sender

---
+++ SJ Foursquare Sensor/src/testApp.cpp
@@ -0,0 +1,134 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	ofSetVerticalSync(true);
+	ofSetFrameRate(30);
+		
+	lastTimeChecked = 0;
+	bChecked = false;
+	
+	venueDetails.setAuth("brenfer@rockwellgroup.com", "kanarick");
+	radiusSearch.setAuth("brenfer@rockwellgroup.com", "kanarick");
+	
+	venueDetails.getVenueDetails("101107");
+	ofBackground(0,0,0);
+	
+	//load host + port
+	ofxXmlSettings settings;
+	bool bLoaded = settings.loadFile("settings.xml");
+	string host = "localhost";
+	int port = 12345;
+	if (bLoaded){
+		settings.pushTag("settings");{
+			host = settings.getValue("host","localhost");
+			port = settings.getValue("port",12345);
+		}settings.popTag();
+	};
+	
+	//setup OSC
+	sender.setup(host, port);	
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+	if ((ofGetElapsedTimeMillis() - lastTimeChecked) > CHECK_TIME*1000){
+		
+		//set lat and long to the venue you queried
+		queryLat = venueDetails.getCurrentVenue()->geoLat;
+		queryLong = venueDetails.getCurrentVenue()->geoLong;
+		
+		//rockwell group, NY
+		venueDetails.getVenueDetails("101107");
+		radiusSearch.getVenues(queryLat,queryLong, 50);
+		lastTimeChecked = ofGetElapsedTimeMillis();
+		bChecked = true;
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	int curY = 45;
+	if (venueDetails.isReady()){
+		string newCheckins = ofToString(venueDetails.getCurrentVenue()->getHereNow());
+		if (venueDetails.getCurrentVenue()->getNewCheckins() > 0){
+			ofSetColor(255,255,0);
+			ofDrawBitmapString("There are "+newCheckins+" people checked into "+venueDetails.getCurrentVenue()->getName()+". NEW CHECKIN!", 20, 30);
+			
+			ofxOscMessage m;
+			m.setAddress("/pluginplay/foursquare");
+			m.addStringArg(venueDetails.getCurrentVenue()->getName());
+			m.addIntArg(venueDetails.getCurrentVenue()->getHereNow());
+			
+		} else {
+			ofSetColor(255,255,255);
+			ofDrawBitmapString("There are "+newCheckins+" people checked into "+venueDetails.getCurrentVenue()->getName(), 20, 30);
+		}
+		vector<FoursquareUser *> * users = venueDetails.getCurrentVenue()->getUsers();
+						
+		//trace people checked in
+		for (int i=0; i< (*users).size(); i++){
+			ofDrawBitmapString((*users)[i]->getName(), 20, curY);
+			curY += 15;
+		};
+		curY += 15;
+	}
+	
+	if (radiusSearch.isReady()){
+		
+		vector<FoursquareVenue *> * venues = radiusSearch.getCurrentVenues();
+		
+		for (int i=0; i< venues->size(); i++){
+			if ((*venues)[i]->getNewCheckins() > 0){
+				cout << "new checkin! at "<<(*venues)[i]->getName()<<endl;
+				ofSetColor(255,255,0);
+				ofDrawBitmapString("There are "+ofToString((*venues)[i]->getHereNow())+" people checked into "+(*venues)[i]->getName()+" nearby. NEW CHECKIN!", 20, curY);
+				
+				ofxOscMessage m;
+				m.setAddress("/pluginplay/foursquare/nearby");
+				m.addStringArg((*venues)[i]->getName());
+				m.addIntArg((*venues)[i]->getHereNow());
+			} else {
+				ofSetColor(255,255,255);
+				ofDrawBitmapString("There are "+ofToString((*venues)[i]->getHereNow())+" people checked into "+(*venues)[i]->getName()+" nearby.", 20, curY);
+			}
+			curY += 15;
+		}
+	}
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
---
+++ SJ Foursquare Sensor/src/testApp.h
@@ -0,0 +1,46 @@
+#ifndef _TEST_APP
+#define _TEST_APP
+
+
+#include "ofMain.h"
+#include "ofxXmlSettings.h"
+#include "ofxOsc.h"
+#include "ofxFourSquare.h"
+
+//City hall
+//http://foursquare.com/venue/61626
+
+//RG
+//http://foursquare.com/venue/101107
+
+//#define CHECK_TIME 18
+#define CHECK_TIME 10
+#define VENUE_ID "101107"
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		
+		int lastTimeChecked;
+	
+		ofxFourSquare venueDetails;
+		ofxFourSquare radiusSearch;
+		ofxXmlSettings currentXML;
+		bool bChecked;
+	
+		string queryLat, queryLong;
+		ofxOscSender sender;
+};
+
+#endif
